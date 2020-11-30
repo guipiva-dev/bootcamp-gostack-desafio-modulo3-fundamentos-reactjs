97afd2dbfbcc7e57ba7f26f67b40b112503c338e
Guilherme Piva <guilherme@buildsolutions.com.br>
2020-11-30 11:36:34 -0300

    Challenge Finalized ✔

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,35 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    if (!uploadedFiles.length) {
+      return;
+    }
 
-    // TODO
+    const data = new FormData();
+    data.append('file', uploadedFiles[0].file, uploadedFiles[0].file.name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const droppedFiles = files.map(
+      file =>
+        ({
+          file,
+          name: file.name,
+          readableSize: filesize(file.size),
+        } as FileProps),
+    );
+
+    console.log(uploadedFiles);
+
+    setUploadedFiles(droppedFiles);
   }
 
   return (
